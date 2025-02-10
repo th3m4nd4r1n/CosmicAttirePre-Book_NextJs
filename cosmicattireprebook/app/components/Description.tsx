7e412d0e4ddfb576479eacc423786ba07ac7bd44
th3m4nd4r1n <mdua_be23@thapar.edu>
2025-02-11 03:04:51 +0530

    fixed for mobile

--- cosmicattireprebook/app/components/Description.tsx
+++ cosmicattireprebook/app/components/Description.tsx
@@ -5,25 +5,25 @@ const Description = () => {
   
     return (
         <>
-            <div className='w-screen h-screen bg-black z-10 flex justify-center items-center'>
+            <div className='w-screen h-[85vh] bg-black z-10 flex justify-center items-center'>
                 <div className='flex justify-center items-center w-[80vw] h-[80vh] border-y-[1px] border-[#D9D9D9] z-50'>
                     <div className='flex items-center'>
 
                     <div className='w-[70vw] h-[79vh] flex flex-col justify-start'>
-                        <h2 className='h-fit font-semibold text-[3vw] font-[balle] uppercase'>Cosmic Attire <br /> <span className='text-[1.5vw]'> presents:</span>
+                        <h2 className='h-fit font-semibold md:text-[3vw] text-[7vw] font-[balle] uppercase'>Cosmic Attire <br /> <span className='md:text-[1.5vw] text-[5vw]'> presents:</span>
                         </h2>
-                        <h1 className='h-fit text-red-600 text-[5vw] font-[k2d]'>
+                        <h1 className='h-fit text-red-600 md:text-[5vw] text-[10vw]  font-[k2d] md:mb-0 mb-5'>
                             ALPHA 1
                         </h1>
-                        <h3 className='text-[1.5vw] uppercase font-[k2d]'>
+                        <h3 className='md:text-[1.5vw] text-[4vw] uppercase font-[k2d]'>
                             sometimes, <span className='text-red-600 font-[k2d] hover:text-white'>3 seconds</span> is all it takes 
                         </h3>
                         <br /><br />
-                        <h4 className='text-[1.2vw] w-[40vw]'>
+                        <h4 className='md:text-[1.2vw] text-[3vw] w-[40vw]'>
                         "The next generation of wearable technology enables instant, effortless communication, seamlessly integrating into daily life. With intuitive controls and advanced features, it allows for real-time connectivity, making interactions more convenient and efficient."
                         </h4>
                         <br /><br /><br />
-<a href="https://payments-test.cashfree.com/forms/alpha-1"><button className='text-[1.5vw] rounded-full w-1/3 bg-red-600 hover:bg-black hover:text-red-600 text-black duration-150 ease-linear '>BOOK NOW</button></a>
+<a href="https://payments-test.cashfree.com/forms/alpha-1"><button className='md:text-[1.5vw] text-[5vw] rounded-full md:w-1/3 w-full bg-red-600 hover:bg-black hover:text-red-600 text-black duration-150 ease-linear '>BOOK NOW</button></a>
                     </div>
                     <Image
                     src="/tap.svg"
--- cosmicattireprebook/app/components/Footer.tsx
+++ cosmicattireprebook/app/components/Footer.tsx
@@ -3,20 +3,20 @@ import Image from 'next/image'
 const Footer = () => {
   return (
     <div className='h-[30vh] bg-[#1f1f1f] flex justify-center items-center '>
-        <div className='flex w-screen justify-around'>
+        <div className='flex flex-wrap-reverse w-screen justify-around'>
           <a href="https://instagram.com/_cosmic_attire"><Image
           src="/insta.svg"
           width={200}
           height={200}
           alt="Instagram"
-          className='h-[7vh]'
+          className='md:h-[7vh] h-[7vh]'
            /></a>
           <a href="mailto:attirecosmic@gmail.com"><Image
           src="/mail.svg"
           width={200}
           height={200}
           alt="Instagram"
-          className='h-[7vh]'
+          className='md:h-[7vh] h-[7vh]'
            /></a>
           <a className='mt-[1%]' href="/terms_condition">Terms and Conditions</a>
           <a className='mt-[1%]' href="/contact">Contact Us</a>
