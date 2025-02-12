f4e1922f8028819c6e86e2b68b5c3e73bbb66d8b
th3m4nd4r1n <mdua_be23@thapar.edu>
2025-02-12 18:33:48 +0530

    mobile changes

--- cosmicattireprebook/app/components/Header.tsx
+++ cosmicattireprebook/app/components/Header.tsx
@@ -4,14 +4,14 @@ import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div className='w-screen bg-black z-50 fixed flex justify-end h-[10vh] items-center'>
-      
-  <h2 className='absolute text-center  text-white animate-pulse w-screen duration-75 font-[k2d] text-[2vw]'>
-    <span className='underline hover:text-red-600'>
-      PRE-SALES</span> ARE NOW LIVE
-    </h2>
-    {/* <Image src="/pre.gif" width={3800} height={94} alt="presales live" /> */}
-  <a href='https://rzp.io/rzp/H5JYxZr' className='z-50'><button  className='z-50 mx-6 text-black text-[1.1vw] font-[balle] bg-red-600 rounded-xl' > <span className='text-transparent'>&gt;</span> BOOK NOW <span className='text-transparent'>&gt;</span> </button></a>
+    <div className='w-screen bg-black z-50 fixed flex md:justify-end justify-between h-[10vh] items-center'>
+
+      <h2 className='md:absolute text-center  text-white animate-pulse md:w-screen w-auto duration-75 font-[k2d] md:text-[2vw]'>
+        <span className='underline hover:text-red-600 md:mx-auto mx-2'>
+          PRE-SALES</span> ARE NOW OPEN
+      </h2>
+      {/* <Image src="/pre.gif" width={3800} height={94} alt="presales live" /> */}
+      <a href='https://rzp.io/rzp/H5JYxZr' className='z-50'><button className='z-50 mx-6 text-black md:text-[1.1vw] md:w-auto font-[balle] bg-red-600 rounded-xl' > <span className='text-transparent'>&gt;</span> BOOK NOW <span className='text-transparent'>&gt;</span> </button></a>
     </div>
   )
 }
--- cosmicattireprebook/app/components/Photo.tsx
+++ cosmicattireprebook/app/components/Photo.tsx
@@ -6,28 +6,28 @@ const Photo = () => {
   return (
     <div className='h-[150vh] flex items-center'>
 
-    <div className='flex justify-center items-center w-screen bg-[rgba(84,84,84,0.16)] h-[40vh]'>
-        <div className='w-screen h-[35vh] bg-transparent border-y-[1px] border-white flex items-center justify-around'>
+    <div className='flex  justify-center items-center w-screen bg-[rgba(84,84,84,0.16)] h-screen md:h-[40vh]'>
+        <div className='w-screen h-[95vh] md:h-[35vh] bg-transparent border-y-[1px] border-white flex md:flex-row flex-col items-center justify-around'>
             <Image
             src="/WEAR.svg"
             width={700}
             height={500}
             alt="band"
-            className='md:h-[25vh] md:w-[25vw]'
+            className='md:h-[25vh] md:w-[25vw] hover:scale-110 duration-200'
             />
             <Image
             src="/tap.svg"
             width={700}
             height={500}
             alt="band"
-            className='md:h-[25vh] md:w-[25vw]'
+            className='md:h-[25vh] md:w-[25vw] hover:scale-110 duration-200'
             />
             <Image
             src="/share.svg"
             width={700}
             height={500}
             alt="band"
-            className='md:h-[25vh] md:w-[25vw]'
+            className='md:h-[25vh] md:w-[25vw] hover:scale-110 duration-200'
             />   
 
            
